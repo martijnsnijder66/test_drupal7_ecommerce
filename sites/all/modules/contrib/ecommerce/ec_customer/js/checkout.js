9aa3613e04a2d1f5a7dff386f44ed80574afb47c
unknown <martijn.snijder@E1638.efocus.local>
2015-05-27 10:57:31 +0200

    Added ecommerce module

---
+++ sites/all/modules/contrib/ecommerce/ec_customer/js/checkout.js
@@ -0,0 +1,31 @@
+(function($) {
+  Drupal.hideShippingAddress = function(hide) {
+    if (hide) {
+      $('.customer-addresses').removeClass('shippable');
+      $('#customer-address-shipping').hide();
+    }
+    else {
+      $('.customer-addresses').addClass('shippable');
+      $('#customer-address-shipping').show();
+    }
+  };
+
+  Drupal.behaviors.ecCustomer = {
+    attach: function() {
+      if ($('#ec-customer-use-for-shipping:checked').size()) {
+        Drupal.hideShippingAddress(true);
+      }
+      else {
+        Drupal.hideShippingAddress(false);
+      }
+      $('#ec-customer-use-for-shipping').click(function() {
+        if (this.checked) {
+          Drupal.hideShippingAddress(true);
+        }
+        else {
+          Drupal.hideShippingAddress(false);
+        }
+      });
+    }
+  };
+})(jQuery);
